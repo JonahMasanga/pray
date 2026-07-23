3f652d440c422e4e7dc143922422264aa1229615
JonahMasanga <43843708+JonahMasanga@users.noreply.github.com>
2026-07-23 22:11:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-//import base44 from "@base44/vite-plugin"
+import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
       hmrNotifier: true,
       navigationNotifier: true,
@@ -16,7 +15,7 @@ export default defineConfig({
     }),
     react(),
   ],
-  // Increase chunk size warning limit to 2000 KB to reduce noisy warnings for larger chunks
+  // Increase chunk size warning limit (in KB) to reduce noisy warnings for larger chunks
   build: {
     chunkSizeWarningLimit: 2000
   }
